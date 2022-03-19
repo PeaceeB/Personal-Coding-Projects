340e9a8e3be815a8989a1833eed016285ea2317a
PeaceeB <101627405+PeaceeB@users.noreply.github.com>
2022-03-19 09:04:16 +0000

    Madlibs
    Create your own story with this short and fun game

---
+++ madlibs_2.py
@@ -0,0 +1,14 @@
+gender = input("Gender: ")
+name = input ("Name: ")
+pronoun = input("Pronoun: ")
+age = input("Age: ")
+height = input ("Height: ")
+verb = input("Verb: ")
+adj = input("Adjective: ")
+
+
+madlib = f"There once was a {gender} named {name}, and {pronoun} was {age} years old \
+and {height} tall. {name} loved to {verb} and {pronoun} did it everyday of the week. {name} \
+found it {adj}!"
+
+print(madlib)
